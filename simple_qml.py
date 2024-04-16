396cfe6364ba74520b47768b2975832216695d3f
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-04-16 12:27:19 +0530

    Update simple_qml.py

--- simple_qml.py
+++ simple_qml.py
@@ -9,6 +9,7 @@ device = qml.device('default.qubit', wires=2, shots=1000)
 def my_quantum_function():
     qml.RZ(0.9, wires=0)
     qml.CNOT(wires=[0,1])
+    qml.Hadamard(wires=0)
     qml.RY(0.3, wires=1)
     return qml.probs(wires=[0,1])
 
@@ -16,7 +17,7 @@ qnode = qml.QNode(my_quantum_function, device)
 print(qml.draw(qnode)())
 
 # output is as shown below
-# 0: ──RZ(0.90)─╭●───────────┤ ╭Probs
+# 0: ──RZ(0.90)─╭●──H────────┤ ╭Probs
 # 1: ───────────╰X──RY(0.30)─┤ ╰Probs
 
 # another way to see the circuit
